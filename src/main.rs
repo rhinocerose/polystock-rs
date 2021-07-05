4b8765a5f4acf6a98a18e0ee79839ea7084d3cac
rhinocerose <39277896+rhinocerose@users.noreply.github.com>
2021-07-05 16:38:54 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,20 @@
 use futures::{ future, StreamExt };
 use yahoo_finance::Streamer;
-use polystock_rs;
+// mod ticker_info;
+mod ticker_info;
 
 #[tokio::main]
 async fn main() {
    let streamer = Streamer::new(vec!["AAPL", "QQQ", "^DJI", "^IXIC"]);
 
-   let ticker = TickerInfo::new("GME");
-   
+   let mut ticker = ticker_info::TickerInfo::new("GME");
+
    streamer.stream().await
       .for_each(|quote| {
          println!("At {} in session {:?}, {} is trading for ${}", quote.timestamp, quote.session, quote.symbol, quote.price);
-         ticker.update_price(quote.price, quote.timestamp);
+         ticker.update_price(quote.price, quote.timestamp).expect("Unable to update ticker");
          future::ready(())
       })
       .await;
-   
-}
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 }
