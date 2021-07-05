ac9ad9d15f5fa14181eb282fe42c4e5f4cca13a4
rhinocerose <39277896+rhinocerose@users.noreply.github.com>
2021-07-05 11:36:08 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use futures::{ future, StreamExt };
+use yahoo_finance::Streamer;
+
+#[tokio::main]
+async fn main() {
+   let streamer = Streamer::new(vec!["AAPL", "QQQ", "^DJI", "^IXIC"]);
+
+   streamer.stream().await
+      .for_each(|quote| {
+         println!("At {} in session {}, {} is trading for ${}", quote.timestamp, quote.session, quote.symbol, quote.price);
+         future::ready(())
+      })
+      .await;
 }
