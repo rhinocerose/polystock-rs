68eb8bcdec6df41045b67628df06b2083068e19b
rhinocerose <39277896+rhinocerose@users.noreply.github.com>
2021-07-05 14:42:46 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,14 @@ use yahoo_finance::Streamer;
 async fn main() {
    let streamer = Streamer::new(vec!["AAPL", "QQQ", "^DJI", "^IXIC"]);
 
+   let ticker = TickerInfo::new("GME")
    streamer.stream().await
       .for_each(|quote| {
          println!("At {} in session {:?}, {} is trading for ${}", quote.timestamp, quote.session, quote.symbol, quote.price);
          future::ready(())
       })
       .await;
+   ticker.update_price(quote.price, quote.timestamp);
 }
 
 #[cfg(test)]
